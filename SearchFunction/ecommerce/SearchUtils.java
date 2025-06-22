d8f71fa5df10596be7d6cb2a138da3ba5f298a24
venkatesan3107 <venkatesan1813@gmail.com>
2025-06-22 22:55:50 +0530

    Add files via upload

---
+++ SearchFunction/ecommerce/SearchUtils.java
@@ -0,0 +1,38 @@
+package ecommerce;
+import java.util.Comparator;
+
+public class SearchUtils {
+
+
+    public static Product linearSearch(Product[] products, String name) {
+        for (Product product : products) {
+            if (product.productName.equalsIgnoreCase(name)) {
+                return product;
+            }
+        }
+        return null;
+    }
+
+    public static Product binarySearch(Product[] products, String name) {
+        int left = 0;
+        int right = products.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int cmp = products[mid].productName.compareToIgnoreCase(name);
+
+            if (cmp == 0) {
+                return products[mid];
+            } else if (cmp < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return null;
+    }
+
+    public static void sortProductsByName(Product[] products) {
+        java.util.Arrays.sort(products, Comparator.comparing(p -> p.productName.toLowerCase()));
+    }
+}
